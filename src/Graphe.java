94ec1c55774f9a7b7100ce7b10f026216032bf51
Franel <franel@debian>
2020-01-13 12:17:28 +0700

    Initialisation du projet

---
+++ src/Graphe.java
@@ -0,0 +1,21 @@
+import java.util.ArrayList;
+
+public class Graphe {
+	protected int nbNoeuds;
+	protected ArrayList<Noeud> noeuds;
+	
+	public Graphe() {
+    //a completer
+	}
+	
+	public Graphe(Noeud[] nds) {
+    //a completer
+	}
+	
+	
+	public void affiche() {
+    //a completer
+	}
+	
+	
+}
---
+++ src/Main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+
+/**
+ * @author franel
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Projet de Recherche Opérationnelle");
+
+	}
+
+}
